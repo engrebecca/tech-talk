d95284bc122bad669fb68d6735bd7857f91d76a1
reng <reng412@gmail.com>
2020-11-16 09:45:47 -0800

    Define user model

---
+++ models/user.js
@@ -0,0 +1,56 @@
+module.exports = function (sequelize, DataTypes) {
+    // Define Tag model
+    var User = sequelize.define("User", {
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        lastName: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        bio: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        // photo: {
+        //     type: DataTypes.STRING,
+        //     allowNull: false
+        // },
+        role: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        organization: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        github: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        website: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        location: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    });
+
+    // Tag.associate = function (models) {
+    //     // A tag has many posts
+    //     models.Tag.hasMany(models.Post);
+    // };
+
+    return User;
+};
