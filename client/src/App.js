a4161fdcab66198eb48ac1f2d9933882cbc2b0d0
Kelly Stone <kellystone916@gmail.com>
2020-11-24 13:30:58 -0800

    add navbar, col, container, row

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+// import Test from "./components/Test";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Row from "./components/Row";
+import Column from "./components/Column";
+import Container from "./components/Container";
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
-      </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
+  <div className= "name">
+    <Navbar></Navbar>
+    <Container></Container>
+    <Column></Column> 
+     <Row></Row>
+   {/* <Test></Test> */}
+   </div>
   );
 }
 
