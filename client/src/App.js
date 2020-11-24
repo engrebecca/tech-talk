04e3333dc87840fb3f4bdc8c3c28b5f18badeec7
reng <reng412@gmail.com>
2020-11-24 13:30:15 -0800

    Add member card

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import Test from "./components/Test";
 import "./App.css";
+import CardMember from "./components/CardMember"
 
 function App() {
   return (
-    <Test></Test>
+    // <Test></Test>
+    <CardMember></CardMember>
   );
 }
 
