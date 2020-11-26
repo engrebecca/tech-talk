3b4f4972318951329d525ff431a9efed4b8c60d1
reng <reng412@gmail.com>
2020-11-25 20:04:52 -0800

    Add props to member page and carduser component to display user information on page

--- client/src/components/Container/index.js
+++ client/src/components/Container/index.js
@@ -3,12 +3,12 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 
-export default function SimpleContainer() {
+export default function SimpleContainer(props) {
   return (
-    <React.Fragment>
-      <CssBaseline />
-      <Container maxWidth="sm">
-        <Typography component="div" style={{ height: '100vh' }} />
+    <React.Fragment {...props}>
+      <CssBaseline {...props} />
+      <Container maxWidth="sm" {...props}>
+        {/* <Typography component="div" style={{ height: '100vh' }} {...props} /> */}
       </Container>
     </React.Fragment>
   );
--- client/src/pages/MemberPage.js
+++ client/src/pages/MemberPage.js
@@ -1,8 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import API from '../utils/API';
+import React, { useState, useEffect } from "react";
+import API from "../utils/API";
+import Navbar from "../components/Navbar";
+import Container from "../components/Container";
+import CardUser from "../components/CardUser";
 
 function MemberPage() {
-
+    const [members, setMembers] = useState([]);
 
     useEffect(() => {
         loadMembers()
@@ -12,12 +15,26 @@ function MemberPage() {
         API.User.getUser()
             .then(res => {
                 console.log(res.data)
+                setMembers(res.data);
             })
             .catch(err => console.log(err));
     }
 
     return (
-        <div></div>
+        <div>
+            <Navbar />
+            {/* <Container>
+                <CardUser />
+            </Container> */}
+            {members.map(member => {
+                return (
+                    // Pass props to the card component to render each user's individual information
+                    <CardUser key={member.id} bio={member.bio} email={member.email} firstName={member.firstName} lastName={member.lastName} loc={member.location} org={member.organization} photo={member.photo} role={member.role} website={member.website} github={member.github}>
+
+                    </CardUser>
+                )
+            })}
+        </div>
     );
 };
 
