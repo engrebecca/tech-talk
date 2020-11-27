9bde835fd62d691b1470d2600f9efa6be80f7f1a
Kelly Kim <kellykim408@gmail.com>
2020-11-27 14:54:43 -0800

    connect navbar links

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,43 @@
-import React, { Component } from "react";
-// import React from "react";
-// import { BrowserRouter as Router, Route } from "react-router-dom";
-import Homepage from "./pages/Homepage";
+// import React, { Component } from "react";
+import React from "react";
+// import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
+// import Navbar from "./components/Navbar";
+import HomePage from "./pages/Homepage";
+// import MembersPage from "./pages/Members";
+// import SignInPage from "./pages/SignIn";
+// import ProfilePage from "./pages/Profile";
+// import LogOutPage from "./pages/LogOut";
 
 function App() {
   return (
     <div>
-        <Homepage></Homepage>
-              
+      {/* <Router> */}
+        {/* <Navbar></Navbar> */}
+        {/* <Switch>
+          <Route path="/" exact> */}
+            <HomePage></HomePage>
+          {/* </Route> */}
+          {/* <Route path="/Members" exact>
+            <MembersPage></MembersPage>
+          </Route> */}
+          {/* <Route path="/SignIn" exact>
+            <SignInPage></SignInPage>
+          </Route>
+          <Route path="/Profile" exact>
+            <ProfilePage></ProfilePage>
+          </Route> */}
+          {/* <Route path="/LogOut" exact>
+            <LogOutPage></LogOutPage>
+          </Route> */}
+          {/* <Route path="*">
+            <Redirect to="/" />
+          </Route>
+        </Switch>
+      </Router> */}
+
+
     </div>
-  
+
   );
 }
 
