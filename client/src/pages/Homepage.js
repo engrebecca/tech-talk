04db0df94fa272a86bcdb20bf3879dcbdbf927e6
Kelly Kim <kellykim408@gmail.com>
2020-11-29 14:27:14 -0800

    clean up homepage. Delete unnecessary code.

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -6,14 +6,12 @@ import React, { Component } from "react";
 import SignIn from "../components/SignIn";
 // import Container from "./components/Container";
 import BtnSignup from "../components/Btn-signup";
-import Navbar from "../components/Navbar";
 
 
 function Homepage() {
   return (
     // <Container>
     <div>
-    <Navbar></Navbar>
     <SignIn></SignIn>
    {/* </Container> */}
     </div>
