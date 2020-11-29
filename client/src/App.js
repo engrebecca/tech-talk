12fce8c6e52ccce185763e5d844ecce5f5334c47
Kelly Stone <kellystone916@gmail.com>
2020-11-29 15:26:21 -0800

    add profile page index.js and navbar for sign in homepage

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import HomePage from "./pages/Homepage";
 import MemberPage from "./pages/MemberPage";
-// import ProfilePage from "./pages/ProfilePage";
+import ProfilePage from "./pages/ProfilePage";
 import SignUpPage from "./pages/SignupPage";
 // import NavbarHP from "./components/NavbarHP"
 
@@ -10,12 +10,11 @@ function App() {
   return (
     <div>
       <Router>
-        {/* <NavbarHP /> */}
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route exact path="/members" component={MemberPage} />
           <Route exact path="/signup" component={SignUpPage} />
-          {/* <Route exact path="/profile" component={ProfilePage} /> */}
+          <Route exact path="/profile" component={ProfilePage} />
           <Route path="*">
             <Redirect to="/" />
           </Route>
