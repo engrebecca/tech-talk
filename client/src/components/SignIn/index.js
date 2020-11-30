d596b4558d85f798f90205b7866a1483687f0195
Kelly Stone <kellystone916@gmail.com>
2020-11-30 13:35:35 -0800

    fixed route on tech talk copyright link

--- client/src/components/SignIn/index.js
+++ client/src/components/SignIn/index.js
@@ -20,7 +20,7 @@ function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
+      <Link color="inherit" href="/">
         Tech Talk
       </Link>{' '}
       {new Date().getFullYear()}
