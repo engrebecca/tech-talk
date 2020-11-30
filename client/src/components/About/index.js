d2b0a774ccf052e35a57bcf7b1ddb653f9fdbf26
unknown <christy.g.lee@gmail.com>
2020-11-29 17:03:43 -0800

    removed useState from imports

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
