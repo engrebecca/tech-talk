4367c491b97409d9341c858166ddda0d0779da80
unknown <christy.g.lee@gmail.com>
2020-11-29 17:02:11 -0800

    added rough about tech talk text

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -8,7 +8,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    height: '80vh',
+    height: '60vh',
   },
   image: {
     backgroundImage: 'url(https://entrepreneur.indiegogo.com/education/wp-content/uploads/sites/4/2019/05/Soft-Skills-for-Crowdfunding.jpeg)',
@@ -36,9 +36,13 @@ export default function About() {
       
       <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
         <div className={classes.paper}>
-        <Typography component="h1" variant="h5">
+        <Typography component="h1" variant="h3">
             About Tech Talk
           </Typography>
+          <Typography component="p" variant="h6">
+          Tech-Talk is a professional network for women in technology. This is the perfect place for ambitious women to connect and make things happen. Whether you are searching for a job opportunity, looking for a mentor to guide you through this bustling industry, or just have questions about the tech field you're in, Tech-Talk is the community to join. We want you to level up personally and professionally.
+          </Typography>
+          
         </div>
       </Grid>
       <Grid item xs={false} sm={4} md={7} className={classes.image} />
