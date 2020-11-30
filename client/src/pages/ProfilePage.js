4d86bcb0ad671df964a798c75681053af2e38948
Kelly Kim <kellykim408@gmail.com>
2020-11-30 10:29:00 -0800

    add styling for profile page

--- client/src/pages/ProfilePage.js
+++ client/src/pages/ProfilePage.js
@@ -6,58 +6,63 @@ import API from "../utils/API";
 import Navbar from "../components/Navbar";
 import Container from "../components/Container";
 import CardUser from "../components/CardUser";
-
 const useStyles = makeStyles((theme) => ({
     root: {
         flexGrow: 1,
     },
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
 }));
-
-function MemberPage() {
+function ProfilePage() {
     const classes = useStyles();
-    const [members, setMembers] = useState([]);
-
+    const [members, setMember] = useState([]);
     useEffect(() => {
-        loadMembers()
+        loadMember()
     }, []);
-
-    function loadMembers() {
+    function loadMember() {
         API.User.getUser()
             .then(res => {
                 console.log(res.data)
-                setMembers(res.data);
+                setMember(res.data);
             })
             .catch(err => console.log(err));
     }
-
     return (
         <div>
             <Navbar />
-            <Container>
+            <Container component="main" maxWidth="sm">
                 <div className={classes.root}>
-                    <Grid
+                    {/* <Grid
                         container
                         spacing={2}
                         direction="row"
                         justify="flex-start"
                         alignItems="flex-start"
-                    >
+                    > */}
                         {/* Create a card to display each member user's info */}
                         {members.map(member => {
                             return (
-                                <Grid item xs={12} sm={6} key={member.id}>
-                                    {/* // Pass props to the card component to render each user's individual information */}
-                                    <CardUser bio={member.bio} email={member.email} firstName={member.firstName} lastName={member.lastName} loc={member.location} org={member.organization} photo={member.photo} role={member.role} website={member.website} github={member.github}>
-
-                                    </CardUser>
-                                </Grid>
+                                <div className={classes.paper}>
+                                    <Grid container spacing={3}>
+                                        <Grid item xs={12} sm={12} key={member.id}>
+                                            {/* // Pass props to the card component to render each user's individual information */}
+                                            <CardUser bio={member.bio} email={member.email} firstName={member.firstName} lastName={member.lastName} loc={member.location} org={member.organization} photo={member.photo} role={member.role} website={member.website} github={member.github}>
+                                            </CardUser>
+                                        </Grid>
+                                    </Grid>
+                                </div>
                             )
                         })}
-                    </Grid>
+                    {/* </Grid> */}
+             
                 </div>
             </Container>
+            
         </div>
     );
 };
-
-export default MemberPage;
+export default ProfilePage;
