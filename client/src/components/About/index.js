a07867a23f15e09f1fcd421f1d94418db17af593
unknown <christy.g.lee@gmail.com>
2020-12-01 14:12:00 -0800

    added font styling to About component

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -9,7 +9,7 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => ({
   root: {
     height: '80vh',
-    marginTop: 20
+    marginTop: 20,
   },
   image: {
     backgroundImage: 'url(https://entrepreneur.indiegogo.com/education/wp-content/uploads/sites/4/2019/05/Soft-Skills-for-Crowdfunding.jpeg)',
@@ -24,6 +24,16 @@ const useStyles = makeStyles((theme) => ({
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
+  },
+  text: {
+    fontFamily: [
+      'Comfortaa',
+      'cursive',
+    ].join(','),
+    height: 200,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center"
   }
 }));
 
@@ -37,10 +47,10 @@ export default function About() {
       
       <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
         <div className={classes.paper}>
-        <Typography component="h1" variant="h3">
+        <Typography component="h1" className={classes.text} variant="h3">
             About Tech Talk
           </Typography>
-          <Typography component="p" variant="h6">
+          <Typography component="p" className={classes.text} variant="h6">
           Tech-Talk is a professional network for women in technology. This is the perfect place for ambitious women to connect and make things happen. Whether you are searching for a job opportunity, looking for a mentor to guide you through this bustling industry, or just have questions about the tech field you're in, Tech-Talk is the community to join. We want you to level up personally and professionally.
           </Typography>
           
