2e9a740205fb2a7de75967e7f5aa099c18a04455
Kelly Stone <kellystone916@gmail.com>
2020-12-01 13:02:51 -0800

    add import from sticky footer index

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -9,6 +9,7 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import { UserContext } from "../../utils/UserContext";
 
+
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
