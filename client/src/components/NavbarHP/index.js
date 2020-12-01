c7eb0ad274f23ff08e86e5361536888e434af3c2
Kelly Stone <kellystone916@gmail.com>
2020-11-30 19:14:53 -0800

    Merge branch 'main' of https://github.com/engrebecca/tech-talk into homepage

--- client/src/components/NavbarHP/index.js
+++ client/src/components/NavbarHP/index.js
@@ -34,7 +34,7 @@ export default function NavbarHP() {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
-            *Logo*
+          <img className = "logo" src="https://i.ibb.co/zSC5kSq/TECH-TALK-21.png" alt="TECH-TALK-21" border="0"></img>
           </Typography>
           <NavLink to="/signup"> Sign Up</NavLink>
         </Toolbar>
