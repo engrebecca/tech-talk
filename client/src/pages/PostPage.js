fd880b61a811bde5a276e2d141e5d6b02f9d11a1
Kelly Stone <kellystone916@gmail.com>
2020-12-01 13:02:38 -0800

    add import for sticky footer pp

--- client/src/pages/PostPage.js
+++ client/src/pages/PostPage.js
@@ -184,7 +184,7 @@ function PostPage() {
                             </Collapse >
                         </Grid>
                     </Card >
-             <StickyFooter />
+            //  <StickyFooter />
 
                 )
             })}
