3012a92577cb5a3406a98e63fb2e6f7ae7ba6a1f
Kelly Kim <kellykim408@gmail.com>
2020-12-01 23:09:00 -0800

    update button color to grey

--- client/src/components/Btn-tags/index.js
+++ client/src/components/Btn-tags/index.js
@@ -3,7 +3,7 @@ import Button from '@material-ui/core/Button';
 
 function Tags(props) {
   return (
-    <Button variant="contained" color="primary" style={{ marginRight: "10px", marginBottom: "15px" }}>
+    <Button variant="contained" color="#9e9e9e" style={{ marginRight: "10px", marginBottom: "15px" }}>
       {props.tags}
     </Button>
   );
