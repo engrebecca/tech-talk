f1b6e392bd00c7725d9226a00f35c2c7bfed1759
Kelly Stone <kellystone916@gmail.com>
2020-12-01 23:33:49 -0800

    fixed margin top add color

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -34,14 +34,14 @@ export default function ButtonAppBar() {
     <div className={classes.root}>
       <AppBar position="static" className={classes.navbar}>
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+          {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
             <MenuIcon />
-          </IconButton>
+          </IconButton> */}
           <Typography variant="h6" className={classes.title}>
-          <img className = "logo" src="https://i.ibb.co/ctthk2R/TECH-TALK-25.png" width="65" height="65" alt="TECH-TALK-10" border="0"></img>
-          <Button><NavLink to="/" exact style={{marginBottom= 40, color: 'black', textDecoration: 'none'}}> Home </NavLink></Button>
-            <Button><NavLink to="/members" style={{marginBottom= 40, color: 'black', textDecoration: 'none'}}> Members </NavLink></Button>
-            <Button> <NavLink to="/profile" style={{marginBottom= 40, color: 'black', textDecoration: 'none'}}> Profile</NavLink></Button>
+          <img className = "logo" style={{marginTop: 20}} src="https://i.ibb.co/ctthk2R/TECH-TALK-25.png" width="65" height="65" alt="TECH-TALK-10" border="0"></img>
+          <Button><NavLink to="/" exact style={{marginBottom: 40, color: 'black', textDecoration: 'none'}}> Home </NavLink></Button>
+            <Button><NavLink to="/members" style={{marginBottom: 40,  color: 'black', textDecoration: 'none'}}> Members </NavLink></Button>
+            <Button> <NavLink to="/profile" style={{marginBottom: 40,  color: 'black', textDecoration: 'none'}}> Profile</NavLink></Button>
           </Typography>
           {/* <NavLink to="/profile"> Profile </NavLink> */}
           <Button onClick={() => {
